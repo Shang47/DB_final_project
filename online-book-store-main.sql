a91200c1dfb84726764aae0d2385f59b392d3c4e
mrlongsword <wenyi@LAPTOP-KO2DFVQ8.localdomain>
2023-01-07 07:29:31 +0800

    修改資料庫多個tables

---
+++ online-book-store-main.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2023-01-07 16:52:01
+-- 伺服器版本： 10.4.18-MariaDB
+-- PHP 版本： 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `online-book-store-main`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(255) NOT NULL COMMENT '暱稱',
+  `email` varchar(255) NOT NULL,
+  `fb網址` varchar(100) NOT NULL,
+  `password` text NOT NULL,
+  `購買方式` text NOT NULL COMMENT '使用者自行輸入匯款，超商，蝦皮',
+  `寄送方式` text NOT NULL COMMENT '使用者自行輸入'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 傾印資料表的資料 `admin`
+--
+
+INSERT INTO `admin` (`id`, `full_name`, `email`, `fb網址`, `password`, `購買方式`, `寄送方式`) VALUES
+(1, 'Elias', 'eliasfsdev@gmail.com', '', '$2y$10$Nqq/y251QX2Ccvb1Ax7hUuMqQSkG3yRLCxN2KPdetnSP3oaXVH70a', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `authors`
+--
+
+CREATE TABLE `authors` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL COMMENT '角色'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 傾印資料表的資料 `authors`
+--
+
+INSERT INTO `authors` (`id`, `name`) VALUES
+(1, 'kokokoko'),
+(2, '李玟毅'),
+(3, 'hank');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL COMMENT '商品名字',
+  `author_id` int(11) NOT NULL COMMENT '角色id',
+  `description` text NOT NULL COMMENT '詳細',
+  `category_id` int(11) NOT NULL COMMENT '作品id',
+  `cover` varchar(255) NOT NULL COMMENT 'photo1',
+  `file` varchar(255) NOT NULL COMMENT 'photo2',
+  `price` int(10) UNSIGNED NOT NULL,
+  `size` char(1) NOT NULL,
+  `廠牌` varchar(20) NOT NULL,
+  `賣家id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 傾印資料表的資料 `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `author_id`, `description`, `category_id`, `cover`, `file`, `price`, `size`, `廠牌`, `賣家id`) VALUES
+(1, 'TTTT', 1, 'DDDDD', 1, '63b6a1bd4a25d1.14084805.png', '63b6a1bd4b3140.45249261.png', 0, '', '', 0),
+(2, '粉紅飛機杯', 2, '<img src=x onerror=alert(1)>', 3, '63b81fd25bf603.74780115.png', '63b81fd25c2d06.04180536.jpg', 0, '', '', 0),
+(3, '保險套', 2, '你知道的', 2, '63b824b888e335.20984534.png', '63b824b8892345.94829494.png', 0, '', '', 0),
+(4, 'test product', 3, 'sss', 2, '63b94c56dfdd90.49338730.jpg', '63b94c56e0d263.27912733.jpg', 1234, 'S', 'NIKE', 1337),
+(5, '18歲純真少女', 3, '陪你度過寂寞的夜晚', 3, '63b950d5878d97.74062845.png', '63b950d5889975.06028390.png', 3000, 'S', '嘉女', 1024);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL COMMENT '作品'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 傾印資料表的資料 `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'CCCCC'),
+(2, '大懶叫'),
+(3, '美好');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
