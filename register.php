867ec97eaa15b829ad9af365e4890c58c9939091
mrlongsword <wenyi@LAPTOP-KO2DFVQ8.localdomain>
2023-01-07 14:24:24 +0800

    完成帳號註冊功能

--- register.php
+++ register.php
@@ -29,7 +29,7 @@ if (!isset($_SESSION['user_id']) &&
     <link rel="stylesheet" href="css/custom.css">
     <!-- Favicon-->
     <link rel="shortcut icon" href="img/favicon.png">
-	<title>Coshop | 登入</title>
+	<title>Coshop | 註冊</title>
 
     <!-- bootstrap 5 CDN-->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
@@ -66,7 +66,7 @@ if (!isset($_SESSION['user_id']) &&
 			<form class="p-5 rounded shadow"
 				style="max-width: 30rem; width: 100%"
 				method="POST"
-				action="php/auth.php">
+				action="php/register.php">
 
 			<h1 class="text-center display-4 pb-5">註冊帳號</h1>
 			<?php if (isset($_GET['error'])) { ?>
@@ -74,6 +74,34 @@ if (!isset($_SESSION['user_id']) &&
 				<?=htmlspecialchars($_GET['error']); ?>
 			</div>
 			<?php } ?>
+
+			<?php if (isset($_GET['success'])) { ?>
+          <div class="alert alert-success" role="alert">
+			  <?=htmlspecialchars($_GET['success']); ?>
+		  </div>
+		<?php } ?>
+
+			<div class="mb-3 ">
+				<label for="exampleInputEmail1" 
+					class="form-label">名字</label>
+				<input type="text" 
+					class="form-control" 
+					name="name" 
+					id="exampleInputEmail1" 
+					aria-describedby="emailHelp"
+					placeholder="藍教桃">
+			</div>
+
+			<div class="mb-3 ">
+				<label for="exampleInputEmail1" 
+					class="form-label">fb連結</label>
+				<input type="text" 
+					class="form-control" 
+					name="fb_link" 
+					id="exampleInputEmail1" 
+					aria-describedby="emailHelp"
+					placeholder="cosplay@example.com">
+			</div>
 
 			<div class="mb-3 ">
 				<label for="exampleInputEmail1" 
@@ -110,7 +138,7 @@ if (!isset($_SESSION['user_id']) &&
 					class="form-label">寄送方式</label>
 				<input type="text" 
 					class="form-control" 
-					name="logistics" 
+					name="delivery_method" 
 					id="exampleInputLogi1" 
 					>
 			</div>
@@ -160,6 +188,6 @@ if (!isset($_SESSION['user_id']) &&
 </html>
 
 <?php }else{
-  header("Location: admin.php");
+  header("Location: admin.php?loggedin=true");
   exit;
 } ?>
